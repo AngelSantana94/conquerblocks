ed794a1fac569f295de9f77e158f950b94a1f8e0
angelsantana <ignaciosantana373@gmail.com>
2025-12-09 20:20:30 +0100

    Corregiddas todas las rutas relativas y build actualizado en su totalidad rama main

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: "/conquerblocks/",
+  base: "/conquerblocks/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        error404: resolve(__dirname, "404.html"),
+        blog: resolve(__dirname, "blog.html"),
+        about: resolve(__dirname, "quienes_somos.html"),
+        desarrollo_blockchain: resolve(__dirname, "blog/desarrollo_blockchain.html"),
+        desarrollo_perfilesjunior: resolve(__dirname, "blog/desarrollo_full-stack_para_perfiles_junior.html"),
+        inteligencia_marketer: resolve(__dirname, "blog/el_desarrollador_full_stack_marketer.html"),
+        inteligencia_artificial: resolve(__dirname, "blog/inteligencia_artificial.html"),
+        master_ciberseguridad: resolve(__dirname, "cursos/master_en_ciberseguridad.html"),
+        inteligencia_artificial: resolve(__dirname, "cursos/master_en_desarrollo _de_inteligencia_artificial.html"),
+        master_fullstack: resolve(__dirname, "cursos/master_en_desarrollo _web_full-stack.html"),
+        master_blockchain: resolve(__dirname, "cursos/master_en_desarrollo_blockchain.html"),
+        politica_cookies: resolve(__dirname, "legal/politica-cookies.html"),
+        politica_privacidad: resolve(__dirname, "legal/politica-privacidad.html"),
+        terminos_condiciones: resolve(__dirname, "legal/terminos-condiciones.html"),
+      },
+    },
+  },
 });
